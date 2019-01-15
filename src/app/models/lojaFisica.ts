d3977fe9b1cbad31063660d37cc38dd708c408e9
Lucas Santos <lucas.lssg@gmail.com>
2019-01-15 11:51:18 -0300

    Issue #10 - Loja Física, criar model, CRUD

---
+++ src/app/models/lojaFisica.ts
@@ -0,0 +1,11 @@
+export class LojaFisica {
+    id?: string;
+
+    nomeFantasia: string;
+    nomeCompleto: string;
+    email: string;
+    senha: string;
+    cnpf: string;
+    endereco: string;
+    telefone: string;
+}
