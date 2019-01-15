538b3575c3df64d8f35484fee5b792803d2c2bf1
Lucas Santos <lucas.lssg@gmail.com>
2019-01-15 11:30:07 -0300

    Issue #9 - Concluida

---
+++ src/app/models/ecommerce.ts
@@ -0,0 +1,12 @@
+export class Ecommerce {
+    id?: string;
+
+    nomeFantasia: string;
+    nomeCompleto: string;
+    email: string;
+    senha: string;
+    cpfCnpj: string;
+    site: string;
+    endereco: string;
+    telefone: string;
+}
