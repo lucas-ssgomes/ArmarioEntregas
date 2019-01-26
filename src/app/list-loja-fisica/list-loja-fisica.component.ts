935cb79db80d5aa8b603f97a69d1bf10f3d1aff2
Lucas Santos <lucas.lssg@gmail.com>
2019-01-26 01:19:29 -0300

    #12 - LojaFisica CRUD >> Concluída

--- src/app/list-loja-fisica/list-loja-fisica.component.ts
+++ src/app/list-loja-fisica/list-loja-fisica.component.ts
@@ -1,15 +1,57 @@
+import { LojaFisica } from './../models/lojaFisica';
 import { Component, OnInit } from '@angular/core';
+import { DbService } from '../servicos/db.service';
 
 @Component({
   selector: 'app-list-loja-fisica',
   templateUrl: './list-loja-fisica.component.html',
-  styleUrls: ['./list-loja-fisica.component.css']
+  styleUrls: ['./list-loja-fisica.component.css'],
+  providers: [DbService],
 })
 export class ListLojaFisicaComponent implements OnInit {
+  lojasFisicas: LojaFisica[];
+  carregando: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private database: DbService) {
+    this.carregarLojaFisica();
   }
 
+  ngOnInit() { }
+
+  private carregarLojaFisica() {
+    this.carregando = true;
+    this.database.listar<LojaFisica>('lojasFisicas')
+      .then(lojasFisicasDB => {
+        this.lojasFisicas = lojasFisicasDB;
+        this.carregando = false;
+      });
+  }
+
+  remover(eid: string) {
+    this.database.remover('lojasFisicas', eid)
+      .then(() => {
+        this.carregarLojaFisica();
+        alert('Loja Física Removida com Sucesso');
+      });
+  }
+
+  editar(lojaFisica) {
+    lojaFisica.editando = true;
+  }
+
+  cancelEdit(lojaFisica) {
+    lojaFisica.editando = false;
+  }
+
+  confirmEdit(lojaFisica) {
+    this.database.atualizar('lojasFisicas', lojaFisica.eid, {
+      nomeFantasia: lojaFisica.nomeFantasia, nomeCompleto: lojaFisica.nomeCompleto,
+      email: lojaFisica.email, senha: lojaFisica.senha, cnpj: lojaFisica.cnpj,
+      endereco: lojaFisica.endereco, telefone: lojaFisica.telefone
+    })
+      .then(() => {
+        this.carregarLojaFisica();
+        alert('Loja Física Atualizada com Sucesso');
+      });
+  }
 }
--- src/app/loja-fisica/loja-fisica.component.ts
+++ src/app/loja-fisica/loja-fisica.component.ts
@@ -1,15 +1,69 @@
+import { LojaFisica } from './../models/lojaFisica';
 import { Component, OnInit } from '@angular/core';
+import { DbService } from '../servicos/db.service';
+import { Ecommerce } from '../models/ecommerce';
 
 @Component({
   selector: 'app-loja-fisica',
   templateUrl: './loja-fisica.component.html',
-  styleUrls: ['./loja-fisica.component.css']
+  styleUrls: ['./loja-fisica.component.css'],
+  providers: [DbService]
 })
 export class LojaFisicaComponent implements OnInit {
+  novaLojaFisica: LojaFisica;
+  lojasFisicas: LojaFisica[];
+  carregando: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private database: DbService) {
+    this.novaLojaFisica = new LojaFisica();
+    this.carregarUsuarios();
   }
 
+  ngOnInit() { }
+
+  private carregarUsuarios() {
+    this.carregando = true;
+    this.database.listar<LojaFisica>('lojasFisicas')
+      .then (lojasFisicasDB => {
+        this.lojasFisicas = lojasFisicasDB;
+        this.carregando = false;
+      });
+  }
+
+  cadastrar() {
+    this.database.inserir('lojasFisicas', this.novaLojaFisica)
+      .then(() => {
+        this.novaLojaFisica = new LojaFisica();
+        this.carregarUsuarios();
+        alert('Loja Física Cadastrada com Sucesso');
+      });
+  }
+
+  remover(eid: string) {
+    this.database.remover('lojasFisicas', eid)
+      .then (() => {
+        this.carregarUsuarios();
+        alert('Loja Física Removida com Sucesso');
+      });
+  }
+
+  editar(lojaFisica) {
+    lojaFisica.editando = true;
+  }
+
+  cancelEdit(lojaFisica) {
+    lojaFisica.editando = false;
+  }
+
+  confirmEdit(lojaFisica: LojaFisica) {
+    this.database.atualizar('lojasFisicas', lojaFisica.id, {
+      nomeFantasia: lojaFisica.nomeFantasia, nomeCompleto: lojaFisica.nomeCompleto,
+      email: lojaFisica.email, senha: lojaFisica.senha, cnpj: lojaFisica.cnpj,
+      endereco: lojaFisica.endereco, telefone: lojaFisica.telefone
+    })
+      .then(() => {
+        this.carregarUsuarios();
+        alert('Loja Física Atualizada com Sucesso');
+      });
+  }
 }
