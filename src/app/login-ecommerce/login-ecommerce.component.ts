8acbd9fba3f286e1bfc2878b8bb1b170943c7479
Lucas Santos <lucas.lssg@gmail.com>
2019-01-28 03:01:41 -0300

    #19 - Layout de login-ecommerce criado

--- src/app/login-ecommerce/login-ecommerce.component.ts
+++ src/app/login-ecommerce/login-ecommerce.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from '../../assets/materialize/js/materialize.min.js';
 
 @Component({
   selector: 'app-login-ecommerce',
@@ -9,7 +10,18 @@ export class LoginEcommerceComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  options = {
+    coverTrigger: false
+   };
+
+  ngOnInit(): void {
+    // Menu Mobile
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, this.options);
+
+    // Dropdown
+    const elems2 = document.querySelectorAll('.dropdown-trigger');
+    const instances2 = M.Dropdown.init(elems2, this.options);
   }
 
 }
