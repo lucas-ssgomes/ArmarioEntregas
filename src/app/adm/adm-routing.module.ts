b08d3ff154f3ce25f2aa952c9c61716f3e2e8163
Lucas Santos <lucas.lssg@gmail.com>
2019-01-31 14:12:12 -0300

    #27 - Corrigindo caminho de rotas alteradas

--- src/app/adm/adm-routing.module.ts
+++ src/app/adm/adm-routing.module.ts
@@ -5,10 +5,20 @@ import { RouterModule } from '@angular/router';
 import { AdmComponent } from './adm.component';
 import { MenuAdmComponent } from './menu-adm/menu-adm.component';
 import { AdmGuard } from './../guards/adm.guard';
+import { ListLojaFisicaComponent } from '../list-loja-fisica/list-loja-fisica.component';
+import { ListEcommerceComponent } from '../list-ecommerce/list-ecommerce.component';
+import { ListClientesComponent } from '../list-clientes/list-clientes.component';
+import { LojaFisicaComponent } from './../loja-fisica/loja-fisica.component';
 
 const admRoutes = [
-  { path: 'loginAdm', component: AdmComponent},
-  { path: 'menuAdm', component: MenuAdmComponent, canActivate: [AdmGuard]}
+  { path: 'loginAdm', component: AdmComponent },
+  { path: 'menuAdm', component: MenuAdmComponent, canActivate: [AdmGuard], children: [
+      { path: 'listClientes', component: ListClientesComponent },
+      { path: 'listEcommerce', component: ListEcommerceComponent },
+      { path: 'lojaFisica', component: LojaFisicaComponent },
+      { path: 'listLojaFisica', component: ListLojaFisicaComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AdmGuard } from './guards/adm.guard';
-
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LoginEcommerceComponent } from './login-ecommerce/login-ecommerce.component';
 import { LoginLojaFisicaComponent } from './login-loja-fisica/login-loja-fisica.component';
 import { ClienteComponent } from './cliente/cliente.component';
-import { ListClientesComponent } from './list-clientes/list-clientes.component';
 import { EcommerceComponent } from './ecommerce/ecommerce.component';
-import { ListEcommerceComponent } from './list-ecommerce/list-ecommerce.component';
-import { LojaFisicaComponent } from './loja-fisica/loja-fisica.component';
-import { ListLojaFisicaComponent } from './list-loja-fisica/list-loja-fisica.component';
-
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -22,14 +15,6 @@ const routes: Routes = [
   { path: 'loginLojaFisica', component: LoginLojaFisicaComponent},
   { path: 'cliente', component: ClienteComponent},
   { path: 'ecommerce', component: EcommerceComponent},
-  // Adm
-  { path: 'listClientes', component: ListClientesComponent, canActivate: [AdmGuard]},
-  // Adm
-  { path: 'listEcommerce', component: ListEcommerceComponent, canActivate: [AdmGuard]},
-  // Adm
-  { path: 'lojaFisica', component: LojaFisicaComponent, canActivate: [AdmGuard]},
-  // Adm
-  { path: 'listLojaFisica', component: ListLojaFisicaComponent, canActivate: [AdmGuard]},
 ];
 
 @NgModule({
